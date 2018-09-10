9734f2690c512dfdbd4472c4b00147180a915ca3
Sergey <sjsoad666@yandex.ru>
2018-09-10 14:47:09 +0300

    no message

--- CustomNavigation/Sources/Transitions/Push.swift
+++ CustomNavigation/Sources/Transitions/Push.swift
@@ -11,20 +11,22 @@ import UIKit
 open class Push: DirectionalTransition {
     
     override public func prepareForAnimation(with fromView: UIView?, and toView: UIView?) {
-        let xPoint = xPosition(for: toView)
-        let yPoint = yPosition(for: toView)
+        let multiplier: CGFloat = reverseTransition ? -1 : 1
+        let xPoint = xPosition(for: toView) * multiplier
+        let yPoint = yPosition(for: toView) * multiplier
         guard reverseTransition else {
             toView?.transform = CGAffineTransform(translationX: xPoint, y: yPoint)
             return }
-        toView?.transform = CGAffineTransform(translationX: -xPoint * xDelta, y: -yPoint * yDelta)
+        toView?.transform = CGAffineTransform(translationX: xPoint * xDelta, y: yPoint * yDelta)
     }
     
     override public func performAnimation(with fromView: UIView?, and toView: UIView?) {
-        let xPoint = xPosition(for: fromView)
-        let yPoint = yPosition(for: fromView)
+        let multiplier: CGFloat = reverseTransition ? 1 : -1
+        let xPoint = xPosition(for: fromView) * multiplier
+        let yPoint = yPosition(for: fromView) * multiplier
         toView?.transform = .identity
         guard reverseTransition else {
-            fromView?.transform = CGAffineTransform(translationX: -xPoint * xDelta, y: -yPoint * yDelta)
+            fromView?.transform = CGAffineTransform(translationX: xPoint * xDelta, y: yPoint * yDelta)
             return }
         fromView?.transform = CGAffineTransform(translationX: xPoint, y: yPoint)
     }
